1aad0c4e1254bdbabaae6ba8e2bebd493d7d7abd
evan <evanbonner99@gmail.com>
2024-10-19 12:34:31 +0100

    Add run step to build.zig

--- build.zig
+++ build.zig
@@ -12,4 +12,12 @@ pub fn build(b: *std.Build) void {
     });
 
     b.installArtifact(exe);
+
+    const run = b.addRunArtifact(exe);
+
+    if (b.args) |args| {
+        run.addArgs(args);
+    }
+
+    b.step("run", "Run the program").dependOn(&run.step);
 }
